7826c368236fcd547a59adcb55d8e8538a0e2002
Lucciffer <51166059+lucciffer@users.noreply.github.com>
2021-05-02 19:59:41 +0530

    added inpainting files

---
+++ inpainting-using-opencv/mask_generator.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+#reading input image
+img = cv2.imread(r'/home/lucciffer/WORK/Machine Learning/CVG/image restoration/restore_this/test_image1.jpeg', 0)
+print(img.shape)
+
+#generating mask
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        if img[i, j] >= 220 and img[i, j] <= 255:
+            img[i, j] = 255
+        else:
+            img[i, j] = 0
+# cv2.imshow('test', img)
+#saving mask image
+cv2.imwrite('outputs/mask_test1.jpg', img)
+cv2.waitKey(0)
+
